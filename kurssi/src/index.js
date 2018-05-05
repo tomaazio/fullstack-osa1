0aab43fbf46be716eda906967e7f0a41327e3723
tomaazio <tomas.kiviharju@gmail.com>
2018-05-05 22:37:19 +0300

    1.1

---
+++ kurssi/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Otsikko = (props) => {
+  return(
+    <div>
+      <h1>{props.nimi}</h1>
+    </div>
+  )
+}
+
+const Sisalto = (props) => {
+  return(
+    <div>
+    <p>{props.osa1} {props.tehtavia1}</p>
+    <p>{props.osa2} {props.tehtavia2}</p>
+    <p>{props.osa3} {props.tehtavia3}</p>
+    </div>
+  )
+}
+
+const Yhteensa = (props) => {
+  return(
+    <div>
+      <p>yhteensä {props.tehtavat} tehtävää</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const kurssi = 'Half Stack -sovelluskehitys'
+  const osa1 = 'Reactin perusteet'
+  const tehtavia1 = 10
+  const osa2 = 'Tiedonvälitys propseilla'
+  const tehtavia2 = 7
+  const osa3 = 'Komponenttien tila'
+  const tehtavia3 = 14
+
+  return (
+    <div>
+      <Otsikko nimi={kurssi} />
+      <Sisalto  osa1={osa1} tehtavia1={tehtavia1}
+                osa2={osa2} tehtavia2={tehtavia2}
+                osa3={osa3} tehtavia3={tehtavia3} />
+      <Yhteensa tehtavat={tehtavia1+tehtavia2+tehtavia3} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
